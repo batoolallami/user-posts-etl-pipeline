42ce6e0eb5ab8ad312eb685de63752d40bf069f4
batoolallami <145492415+batoolallami@users.noreply.github.com>
2026-04-17 13:00:15 +0100

    Create merge_and_aggregate.py

---
+++ merge_and_aggregate.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def merge_post_user(user_df:pd.DataFrame,post_df:pd.DataFrame)-> pd.DataFrame:
+    merge_df=post_df.merge(user_df,on='user_id',how='left')
+    logging.info(f"Merged dateframe: {(len(merge_df))} rows")
+    return merge_df
+
+def aggregate_df(merge_df:pd.DataFrame) ->pd.DataFrame:
+    result_df=(
+        merge_df.groupby(['user_id','name','city'], as_index=False)
+        .agg(
+            total_post=("post_id","count"),
+            avg_title_length=("title_length","mean")
+            )
+    
+    )
+    logging.info(f"Aggregated summary: {len(result_df)} rows")
+
+    return result_df
