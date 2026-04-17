3df22d0fe8c22e30a0c16c1641f1752c169c4d31
batoolallami <145492415+batoolallami@users.noreply.github.com>
2026-04-17 12:39:04 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+Db_url="postgresql://postgres:YOUR_PASSWORD@localhost:5432/YOUR_DATABASE"
+pipeline_name = "user_post_summary_pipeline"
+users_api_url ="https://jsonplaceholder.typicode.com/users"
+posts_api_url="https://jsonplaceholder.typicode.com/posts"
